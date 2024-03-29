4b39c13d3326ce91edd19c8b4f8b0b82d1f9ebde
İpek Nur Günümdoğdu <90203389+devcraftipek@users.noreply.github.com>
2024-03-29 14:15:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Uygulama: Asal Sayı
+           // Girilen bir sayının asal olup olmadığını kontrol ediniz.
+           // Bir sayı, sadece 1'e ve kendisine tam bölünebiliyorsa asal sayıdır.
+
+            bool asalmi = true;
+
+            Console.Write("sayı: ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            if (sayi==1)
+                asalmi = false;
+
+            for (int i = 2; i < sayi; i++)           
+            {
+                if(sayi%i==0)
+                {
+                    asalmi = false;
+                    break;
+                }
+            }
+
+            if (asalmi)
+                Console.WriteLine("sayı asaldır.");
+            else
+                Console.WriteLine("sayı asal değildir");
+
+        }
+    }
+}
